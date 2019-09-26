2f4485a68603e80081f056693f7feddfda32ba2c
bdhwan <bdhwan@gmail.com>
2019-09-26 10:38:30 +0900

    fix version

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-docker build  --no-cache --tag bdhwan/pm2-api-server:hah .
-docker push bdhwan/pm2-api-server:hah
+docker build  --no-cache --tag bdhwan/pm2-api-server:0.0.3 .
+docker push bdhwan/pm2-api-server:0.0.3
