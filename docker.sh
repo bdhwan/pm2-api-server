77b2995668fd2bca5ff9e23899f4a4764ea9f4f8
bdhwan <bdhwan@gmail.com>
2019-11-01 10:25:53 +0900

    change healthcheck port to env

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-docker build  --no-cache --tag bdhwan/pm2-api-server:0.0.6 .
-docker push bdhwan/pm2-api-server:0.0.6
+docker build  --no-cache --tag bdhwan/pm2-api-server:0.0.7 .
+docker push bdhwan/pm2-api-server:0.0.7
